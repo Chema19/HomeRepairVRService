51c3a2acc1c6acd695cd2bc2e8d834ac713b8806
Chema19 <chemalonso96@gmail.com>
2020-09-20 19:20:56 -0500

    first commit

---
+++ HomeRepairVR/MakeSolution.HomeRepairVR.Service/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MakeSolution.HomeRepairVR.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HomeRepairVR/MakeSolution.HomeRepairVR.Service/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MakeSolution.HomeRepairVR.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
